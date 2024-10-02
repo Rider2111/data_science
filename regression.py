6f0816cd102f7613902228384260789c36108adc
Prashant Dagar <prashant.p.d.2111@gmail.com>
2024-10-02 23:35:59 +0530

    [updated] added slope and intercept formula

--- regression.py
+++ regression.py
@@ -1,22 +1,24 @@
-#!/bin/python3
-
 import math
 from numpy import ndarray, array
 
 def get_slope_and_intercept(xs: ndarray, ys: ndarray):
     if xs.size != ys.size:
-        raise Exception(f"Length doesn't match. len(xs): {nxs}. len(ys): {nys}")
-    i = 0
+        raise Exception(f"Length doesn't match. xs.size: {xs.size}. ys.size: {ys.size}")
+    n = xs.size
+    sum_x = xs.sum()
+    sum_y = ys.sum()
     sum_xy = 0
     sum_x2 = 0
+    i = 0
     while (i < xs.size):
         sum_xy += xs[i] * ys[i]
         sum_x2 += math.pow(xs[i], 2)
         i += 1
-    print(xs.sum())
-    print(ys.sum())
-    print(sum_xy)
-    print(sum_x2)
+    m = (n*sum_xy - sum_x*sum_y)/(n*sum_x2 - math.pow(sum_x,2))
+    b = (sum_y - m*sum_x)/n
+    return (m,b)
+    
+    
 
 xs = [1, 2, 3, 4, 5, 6, 7]
 ys = [1.5, 3.8, 6.7, 9.0, 11.2, 13.6, 16]
