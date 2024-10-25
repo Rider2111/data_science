f802ed599a6c89ed09ba65859f30c2507d9b390a
vishesh <visheshmukherjee.vm@gmail.com>
2024-10-26 00:08:55 +0530

    [update] added logic for size

--- database_importer.py
+++ database_importer.py
@@ -5,10 +5,20 @@ connection = con.connect(host='localhost', user='root')
 cursor = connection.cursor()
 
 database_name = input("Enter database name:")
-cursor.execute(f"CREATE DATABASE {database_name}")
+cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database_name}")
+
 file_path = input("Enter csv file path:")
 file = open(file_path,'r')
+
 first_line = file.readline()
-secon_line = file.readline()
+
+data = []
+for _ in first_line.split(","):
+    data.append(0)
+for line in file:
+    for index, element in enumerate(line.split(",")):
+        if len(element) > data[index]:
+            data[index] = len(element)
+
 cursor.close()
 connection.close()
