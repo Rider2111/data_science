9cb2bbc3f6c808655bebad5278e71b38137e4b05
vishesh <visheshmukherjee.vm@gmail.com>
2024-11-15 22:37:21 +0530

    [update] logistic regression

--- logistic_regression/attendance.py
+++ logistic_regression/attendance.py
@@ -1,3 +1,21 @@
 #!/bin/python3
 
-print("Hello, World !!")
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+
+file_path = "data.csv"
+data_frame = pd.read_csv(file_path)
+print(data_frame.head())
+print(data_frame.describe())
+
+
+target = data_frame['Admitted'].map({"Yes":1, "No":0})
+inputs = data_frame['SAT']
+plt.scatter(inputs,target)
+plt.show()
+
+
+sm.add_constant(inputs)
+logistic_regression = sm.Logit(target, inputs).fit()
+print(logistic_regression.summary())
