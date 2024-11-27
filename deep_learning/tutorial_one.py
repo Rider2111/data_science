9bf813b31a8e1fd48b5634bddd0f18bfb5d39550
vishesh <visheshmukherjee.vm@gmail.com>
2024-11-27 22:42:47 +0530

    [update] updated tutorial one

--- deep_learning/tutorial_one.py
+++ deep_learning/tutorial_one.py
@@ -14,9 +14,20 @@ noise = np.random.uniform(-1, 1, (observations, 1))
 targets = 2*xs + 3*zs + 5 + noise
 init_range = 0.1
 
+
+# Initial values
 weights = np.random.uniform(-init_range, init_range, size=(2,1))
 biases = np.random.uniform(-init_range, init_range, size=1)
-
 learning_rate = 0.02
 
-print(targets)
+for _ in range(100):
+    outputs = np.dot(inputs, weights) + biases
+    deltas = outputs - targets
+    loss = np.sum(deltas ** 2) / 2 / observations
+    print(loss)
+    deltas_scaled = deltas/observations
+    weights = weights - learning_rate*np.dot(inputs.T, deltas_scaled)
+    biases = biases - learning_rate*np.sum(deltas_scaled)
+
+print(weights)
+print(biases)
