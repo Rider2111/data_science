4ff3e9c828f9275c319862b73e4a9c3d748e82b8
vishesh <visheshmukherjee.vm@gmail.com>
2024-12-10 23:55:13 +0530

    [update] updated tutorial one

--- deep_learning/tutorial_one.py
+++ deep_learning/tutorial_one.py
@@ -1,8 +1,9 @@
 #!/bin/python3
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-observations = 1000
+observations = 100
 
 xs = np.random.uniform(-10, 10, size=(observations,1))
 zs = np.random.uniform(-10, 10, size=(observations,1))
@@ -12,22 +13,27 @@ inputs = np.column_stack((xs, zs))
 noise = np.random.uniform(-1, 1, (observations, 1))
 
 targets = 2*xs + 3*zs + 5 + noise
+
 init_range = 0.1
-
-
-# Initial values
 weights = np.random.uniform(-init_range, init_range, size=(2,1))
 biases = np.random.uniform(-init_range, init_range, size=1)
+
 learning_rate = 0.02
 
-for _ in range(100):
+for _ in range(10000):
     outputs = np.dot(inputs, weights) + biases
     deltas = outputs - targets
     loss = np.sum(deltas ** 2) / 2 / observations
-    print(loss)
     deltas_scaled = deltas/observations
     weights = weights - learning_rate*np.dot(inputs.T, deltas_scaled)
     biases = biases - learning_rate*np.sum(deltas_scaled)
+    # print(f"Current loss {loss}")
 
 print(weights)
 print(biases)
+
+# Should be near 45 degree
+plt.plot(outputs, targets)
+plt.xlabel("outputs")
+plt.ylabel("targets")
+plt.show()
